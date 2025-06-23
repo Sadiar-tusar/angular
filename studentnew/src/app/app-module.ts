63c4ff43d393579254dfdc0499191383ce35ddd8
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-06-24 01:36:23 +0600

    angular student

---
+++ studentnew/src/app/app-module.ts
@@ -0,0 +1,35 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Home } from './home/home';
+import { ViewAllStudent } from './view-all-student/view-all-student';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { Addstudent } from './addstudent/addstudent';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    App,
+    Home,
+    ViewAllStudent,
+    Addstudent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch()
+    )
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ studentnew/src/app/view-all-student/view-all-student.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../service/student.service';
+import { Student } from '../../model/student.model';
+
+@Component({
+  selector: 'app-view-all-student',
+  standalone: false,
+  templateUrl: './view-all-student.html',
+  styleUrl: './view-all-student.css'
+})
+export class ViewAllStudent implements OnInit{
+  students: any;
+
+  constructor(private studentservice:StudentService){}
+  ngOnInit(): void {
+   this.loadAllStudent();
+  }
+  loadAllStudent(){
+    this.students = this.studentservice.getAllStudent();
+  }
+
+}
