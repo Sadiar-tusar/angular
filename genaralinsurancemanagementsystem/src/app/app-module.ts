af89b5fc4e34099b2c69ae4b532980db278627ea
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-06-29 13:08:47 +0600

    Project Insurance

---
+++ genaralinsurancemanagementsystem/src/app/app-module.ts
@@ -0,0 +1,26 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Home } from './component/home/home';
+import { Dashboard } from './component/dashboard/dashboard';
+
+@NgModule({
+  declarations: [
+    App,
+    Home,
+    Dashboard
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
