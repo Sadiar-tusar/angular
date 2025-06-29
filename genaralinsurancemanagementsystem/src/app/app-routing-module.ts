937175a0fb286d538fc39472a461cf39587f4d5a
Sadiar-tusar <sadiar.rahman970@gmail.com>
2025-06-29 19:03:28 +0600

    add

--- genaralinsurancemanagementsystem/src/app/app-routing-module.ts
+++ genaralinsurancemanagementsystem/src/app/app-routing-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './component/home/home';
+import { Dashboard } from './component/dashboard/dashboard';
 
 const routes: Routes = [
-  {path:'',component: Home}
+  {path:'',component: Home},
+  {path:'dashboard', component:Dashboard}
 ];
 
 @NgModule({
